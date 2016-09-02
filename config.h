fbfc2dbb6e787aaf8183782c67b4a6894e10cc5c
liminghao <liminghao@xiaomi.com>
2016-09-02 17:29:50 +0800

    openssh: resolve to build break.
    N/A
    
    In file included from external/openssh/ssh-keygen.c:38:
    bionic/libc/include/unistd.h:100:20: error: expected ')'
        __attribute__((__sentinel__(1)));
    
    Signed-off-by: liminghao <liminghao@xiaomi.com>

--- config.h
+++ config.h
@@ -195,6 +195,7 @@
 
 /* OpenBSD's gcc has sentinel */
 /* #undef HAVE_ATTRIBUTE__SENTINEL__ */
+#define HAVE_ATTRIBUTE__SENTINEL__ 1
 
 /* Define to 1 if you have the `aug_get_machine' function. */
 /* #undef HAVE_AUG_GET_MACHINE */
